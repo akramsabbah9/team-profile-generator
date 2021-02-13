5559022a1ae9e969ef03f8c15dc5125bfa8b36db
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-12 19:50:54 -0800

    wrote constructor, added more tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,3 +7,27 @@ test("creates an employee object", () => {
     expect(employee.id).toBe("0940");
     expect(employee.email).toBe("test@test.com");
 });
+
+test("gets employee's name", () => {
+    const employee = new Employee("Benjamin", "0940", "test@test.com");
+
+    expect(employee.getName()).toBe("Benjamin");
+});
+
+test("gets employee's ID", () => {
+    const employee = new Employee("Benjamin", "0940", "test@test.com");
+
+    expect(employee.getId()).toBe("0940");
+});
+
+test("gets employee's email", () => {
+    const employee = new Employee("Benjamin", "0940", "test@test.com");
+
+    expect(employee.getEmail()).toBe("test@test.com");
+});
+
+test("employee's role is 'employee'", () => {
+    const employee = new Employee("Benjamin", "0940", "test@test.com");
+
+    expect(employee.getRole()).toBe("Employee");
+});
