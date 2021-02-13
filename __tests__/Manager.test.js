c476ad320f2f5de2aa1cb4bdc56c33cd13952e72
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-12 20:05:01 -0800

    wrote manager constructor and more tests

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,10 +1,30 @@
 const Manager = require("../lib/Manager");
 
 test("create manager object", () => {
-    const manager = ("Smith", "1657", "testm@test.com", "777");
+    const manager = new Manager("Smith", "1657", "testm@test.com", "777");
 
     expect(manager.name).toBe("Smith");
     expect(manager.id).toBe("1657");
     expect(manager.email).toBe("testm@test.com");
     expect(manager.officeNumber).toBe("777");
+});
+
+test("gets manager's name, ID, and email", () => {
+    const manager = new Manager("Smith", "1657", "testm@test.com", "777");
+
+    expect(manager.getName()).toBe("Smith");
+    expect(manager.getId()).toBe("1657");
+    expect(manager.getEmail()).toBe("testm@test.com");
+});
+
+test("gets manager's office number", () => {
+    const manager = new Manager("Smith", "1657", "testm@test.com", "777");
+
+    expect(manager.getOfficeNumber()).toBe("777");
+});
+
+test("gets manager's role", () => {
+    const manager = new Manager("Smith", "1657", "testm@test.com", "777");
+
+    expect(manager.getRole()).toBe("Manager");
 });
