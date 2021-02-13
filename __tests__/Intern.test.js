039c2636e2e2307c084d5f37863598f77e6bdd0f
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-12 20:37:24 -0800

    fixed tests and added intern getter methods

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,7 @@
-const Engineer = require("../lib/Engineer");
+const Intern = require("../lib/Intern");
 
 test("creates intern object", () => {
-    const intern = new Engineer("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
+    const intern = new Intern("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
 
     expect(intern.name).toBe("Timmy");
     expect(intern.id).toBe("7599");
@@ -10,7 +10,7 @@ test("creates intern object", () => {
 });
 
 test("gets an intern's name, ID, and email", () => {
-    const intern = new Engineer("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
+    const intern = new Intern("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
 
     expect(intern.getName()).toBe("Timmy");
     expect(intern.getId()).toBe("7599");
@@ -18,13 +18,13 @@ test("gets an intern's name, ID, and email", () => {
 });
 
 test("gets an intern's school", () => {
-    const intern = new Engineer("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
+    const intern = new Intern("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
 
     expect(intern.getSchool()).toBe("Fake School of Forgery");
 });
 
 test("gets an intern's role", () => {
-    const intern = new Engineer("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
+    const intern = new Intern("Timmy", "7599", "testi@test.com", "Fake School of Forgery");
 
     expect(intern.getRole()).toBe("Intern");
 });
