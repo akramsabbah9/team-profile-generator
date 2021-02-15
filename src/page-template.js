8473c0088babd772c6fdce6681a20aa7c913ce10
Akram Sabbah <akramsabbah9@gmail.com>
2021-02-14 18:48:04 -0800

    populates cards with team information

--- src/page-template.js
+++ src/page-template.js
@@ -4,29 +4,38 @@
 const generateCard = employee => {
     // get role-specific attributes
     var roleAttr;
+    var icon;
     switch(employee.getRole()) {
         case "Manager":
-            roleAttr = { attr: "Office Number", val: employee.getOfficeNumber() }
+            roleAttr = `<li class="list-group-item">Office Number: ${employee.getOfficeNumber()}</li>`;
+            icon = `<i class="fas fa-mug-hot"></i>`;
             break;
         case "Engineer":
-            roleAttr = { attr: "GitHub", val: employee.getGithub() }
+            roleAttr = `<li class="list-group-item">GitHub: <a href="https://github.com/${employee.getGithub()}" target="_blank">${employee.getGithub()}</a></li>`;
+            icon = `<i class="fas fa-glasses"></i>`;
             break;
         case "Intern":
-            roleAttr = { attr: "School", val: employee.getSchool() }
+            roleAttr = `<li class="list-group-item">School: ${employee.getSchool()}</li>`;
+            icon = `<i class="fas fa-user-graduate"></i>`;
             break;
         default:
-            roleAttr = { attr: "Error", val: "" }
+            roleAttr = ``;
+            icon = ``;
     }
 
-    return `<div class="card mt-4 ml-3 mr-3">
-    <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>`;
-    
+    return `        <div class="card mb-4 ml-3 mr-3">
+            <div class="card-header bg-primary text-white">
+                <h5 class="card-title">${employee.getName()}</h5>
+                <h6 class="card-subtitle mb-2">${icon} ${employee.getRole()}</h6>
+            </div>
+            <div class="card-body bg-light">
+                <ul class="list-group mt-3 mb-3">
+                    <li class="list-group-item">ID: ${employee.getId()}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}" target="_blank">${employee.getEmail()}</a></li>
+                    ${roleAttr}
+                </ul>
+            </div>
+        </div>`;
 };
 
 // make cards for every employee in data and append them
@@ -53,6 +62,7 @@ const generateTeamPage = data => {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
     <title>My Team</title>
     <link rel="stylesheet" href="style.css">
 </head>
@@ -62,7 +72,7 @@ const generateTeamPage = data => {
         <h1>My Team<h1>
     </header>
     <div class="d-flex justify-content-center flex-wrap">
-        ${generateTeam(data)}
+${generateTeam(data)}
     </div>
 </body>
 </html>`;
